80691bb8406a8f87d55d6c13b63e7d2ac9710b51
Jadhuk Herlambang <jadhuk11@gmail.com>
2021-08-05 15:53:48 +0700

    category

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import Headphones from "./pages/category/headphones/Headphones";
+import Earphones from "./pages/category/earphones/Earphones";
+import Speakers from "./pages/category/speakers/Speakers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/headphones" component={Headphones} />
+            <Route exact path="/earphones" component={Earphones} />
+            <Route exact path="/speakers" component={Speakers} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/category/earphones/Earphones.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import CategoryContainer from "../categoryContainer/CategoryContainer";
+
+import styles from "./earphone.module.css";
+import earphone1 from "../../../components/assets/category-earphones/desktop/image-yx1-earphones.jpg";
+
+const Earphones = () => {
+  return (
+    <div>
+      {/* ----HEADER---- */}
+      <div className={styles.header}>
+        <h2 className={styles.text__header}>EARPHONES</h2>
+      </div>
+
+      {/* ----ITEM 1---- */}
+      <div className={styles.item}>
+        <div className={styles.pict__container}>
+          <img className={styles.pict} src={earphone1} />
+          <div className={styles.shadow} />
+        </div>
+        <div className={styles.description__container}>
+          {/* TITLE NEW */}
+          <p className={styles.title__new}>NEW PRODUCT</p>
+
+          {/* TITLE */}
+          <div className={styles.title}>
+            <p>XX99 Mark II Headphones</p>
+          </div>
+          {/* DESCRIPTION */}
+          <div className={styles.description}>
+            <p>
+              The new XX99 Mark II headphones is the pinnacle of pristine audio.
+              It redefines your premium headphone experience by reproducing the
+              balanced depth and precision of studio-quality sound.
+            </p>
+          </div>
+          <div>
+            <button className={styles.btn}>See Product</button>
+          </div>
+        </div>
+      </div>
+
+      <CategoryContainer />
+    </div>
+  );
+};
+
+export default Earphones;
