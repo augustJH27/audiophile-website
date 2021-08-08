64f482b94f85b4ffba3ba01f7c7f930a0d9a37a1
Joy Agustian <Pro@Joy-Agustian.local>
2021-08-08 17:01:32 +0700

    navbar done burger not yet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
+import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 import Bringing from './components/Bringing/Bringing';
 import Hero2 from './components/Hero2/Hero2';
+import Catalog from './components/CatalogCard/Catalog';
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,15 +18,34 @@ function App() {
     <Router>
       <Switch>
         <Route exact path='/'>
+        <Navbar />
           <Hero />
+          <Container>
+          <Catalog />
           <Hero2 />
           <Bringing />
-          <Footer />
+          {/* <Footer /> */}
+          </Container> 
         </Route>
       </Switch>
     </Router>
 
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 7.5rem;
+  margin-bottom: 7.5rem;
+  @media screen and (min-width: 768px) {
+    gap: 6rem;
+    margin-bottom: 6rem;
+  }
+  @media screen and (min-width: 1280px) {
+    gap: 10.5rem;
+    margin-bottom: 12.5rem;
+  }
+`;
 
 export default App;
